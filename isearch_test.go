e0a5b0d442066df4656d6268f1673a7cd24dbf67
martin.haesler <martin.haesler@gmail.com>
2014-03-17 19:43:42 +0000

    Add isearch_test file.

---
+++ isearch_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+        "testing"
+)
+
+var rstring1 = "\"987 http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.jpg Silk: Series 3 Episode 1"
+
+var rstring2 = "\"988 http://www.bbc.co.uk/iplayer/images/episode/b03xkk94_150_84.jpg Silk: Series 3 Episode 2"
+
+func TestIndex(t *testing.T) {
+        t1 := &Ipsearch{searchterm: rstring1}
+        t2 := &Ipsearch{searchterm: rstring2}
+        indext1 := t1.Index()[0]
+        if indext1 != "987" {
+                t.Error("Expted \"987\" got: ", indext1)
+        } else if indext2 := t2.Index()[0]; indext2 != "988" {
+                t.Error("expteced \"988\" got: ", indext2)
+        }
+}
+
+// test Thumbmail method
+func TestThumbnail(t *testing.T) {
+        t1 := &Ipsearch{searchterm: rstring1}
+        thumbt1 := t1.ThumbNail()[0]
+        if thumbt1 != "http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.jpg" {
+                t.Error("Expected some thumbnail, got ", thumbt1)
+        }
+}
