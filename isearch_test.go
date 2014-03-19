9331fc97c4272b27c135dc6f51e205c3cdc8bf4d
martin.haesler <martin.haesler@gmail.com>
2014-03-19 21:14:28 +0000

    Modifiy testThumbnail fn to use thumbnail() from isearch

--- isearch_test.go
+++ isearch_test.go
@@ -8,16 +8,6 @@ var rstring1 = "987 http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.
 
 var rstring2 = "988 http://www.bbc.co.uk/iplayer/images/episode/b03xkk94_150_84.jpg Silk: Series 3 Episode 2"
 
-// func TestIndex(t *testing.T) {
-//         t1 := &Ipsearch{searchterm: rstring1}
-//         t2 := &Ipsearch{searchterm: rstring2}
-//         indext1 := t1.Index()[0]
-//         if indext1 != "987" {
-//                 t.Error("Expted \"987\" got: ", indext1)
-//         } else if indext2 := t2.Index()[0]; indext2 != "988" {
-//                 t.Error("expteced \"988\" got: ", indext2)
-//         }
-// }
 func TestIndex(t *testing.T) {
         t1 := index(rstring1)
         t2 := index(rstring2)
@@ -28,11 +18,13 @@ func TestIndex(t *testing.T) {
         }
 }
 
-// test Thumbmail method
-// func TestThumbnail(t *testing.T) {
-//         t1 := &Ipsearch{searchterm: rstring1}
-//         thumbt1 := t1.ThumbNail()[0]
-//         if thumbt1 != "http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.jpg" {
-//                 t.Error("Expected some thumbnail, got ", thumbt1)
-//         }
-// }
+func TestThumbnail(t *testing.T) {
+        t1 := thumbnail(rstring1)
+        t2 := thumbnail(rstring2)
+
+        if t1 != "http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.jpg" {
+                t.Error("Expected http://www.bbc.co.uk/iplayer/images/episode/b03wtmlq_150_84.jpg, got ", t1)
+        } else if t2 != "http://www.bbc.co.uk/iplayer/images/episode/b03xkk94_150_84.jpg" {
+                t.Error("Expected http://www.bbc.co.uk/iplayer/images/episode/b03xkk94_150_84.jpg, got ", t2)
+        }
+}
