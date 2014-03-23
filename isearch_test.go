7432cb60112cbc161cba60f85e21f7dbfe1583c4
martin.haesler <martin.haesler@gmail.com>
2014-03-23 12:12:26 +0000

    Change test index() method to reflect changes.

--- isearch_test.go
+++ isearch_test.go
@@ -11,10 +11,10 @@ var rstring2 = "988 http://www.bbc.co.uk/iplayer/images/episode/b03xkk94_150_84.
 func TestIndex(t *testing.T) {
         t1 := index(rstring1)
         t2 := index(rstring2)
-        if t1 != "987" {
-                t.Error("Expected 987 got: ", t1)
-        } else if t2 != "988" {
-                t.Error("Expected 988 got: ", t2)
+        if t1 != "/info?index=987" {
+                t.Error("Expected /info?index=987 got: ", t1)
+        } else if t2 != "/info?index=988" {
+                t.Error("Expected ?info?index=988 got: ", t2)
         }
 }
 
@@ -36,17 +36,5 @@ func TestTitle(t *testing.T) {
                 t.Error("Expeced Silk: Series 3 Episode 1, got: ", t1)
         } else if t2 != "Silk: Series 3 Episode 2" {
                 t.Error("Expected Silk: Series 3 Episode 2, got: ", t2)
-        }
-}
-
-func TestCategory(t *testing.T) {
-        var t1 = Categoryinit()
-        t2 := Categoryinit()
-        t1name := t1[0]
-        t2first := t2[0]
-        if t1name.Name != "popular" {
-                t.Error("Expected <popular>, got: ", t1name.Name)
-        } else if t2first.Url != "/?category=popular" {
-                t.Error("Expected <?category=popular>, got: ", t2first.Url)
         }
 }
