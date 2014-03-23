af4239cc56441dc9980df3ec9c493531c7be767a
martin.haesler <martin.haesler@gmail.com>
2014-03-23 09:45:10 +0000

    Add route for results.

--- routes.go
+++ routes.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+        "fmt"
         "github.com/codegangsta/martini"
         "github.com/martini-contrib/render"
+        "net/http"
 )
 
+type result struct {
+        searchvalue string `form:"searchvalue"`
+}
+
 func RunServer() {
-        cats := Categoryinit()
+        //        cats := Categoryinit()
         m := martini.Classic()
         m.Use(render.Renderer(render.Options{Layout: "layout",
                 Directory: "templates"}))
         m.Get("/", func(r render.Render) {
-                r.HTML(200, "index", cats)
+                r.HTML(200, "index", "index")
         })
         m.Get("/about", func(r render.Render) {
-                r.HTML(200, "about", cats)
+                r.HTML(200, "about", "about")
 
         })
-        // m.Get("/about", func(r render.Render) {
-        //         return "Hallo"
-        // })
 
+        m.Post("/results", func(r render.Render, re *http.Request) {
+                sv := re.FormValue("searchvalue")
+                resmap := map[string]string{"category": "",
+                        "searchvalue": sv}
+                out := NewSearch(resmap)
+                fmt.Println(out)
+                r.HTML(200, "result", out)
+        })
         m.Run()
 }
